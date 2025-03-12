ebe55bd641a19469062b2ee29d70e7ed7bba0d5d
Fernando Mejia Laguna <mejia190899@gmail.com>
2025-03-11 20:50:51 -0600

    Created view for notes.

--- google_keep/views.py
+++ google_keep/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Note, Tag   
 
 # Create your views here.
+def note_list(request):
+    """View for showing all notes"""
+    notes = Note.objects.all()
+    return render(request, 'notes/note_list.html',{'notes': notes})
