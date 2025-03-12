6fb9d96b9b755eafcfde00f50bc199f46400cbf9
Fernando Mejia Laguna <mejia190899@gmail.com>
2025-03-11 20:37:21 -0600

    Added app dir to settings.py  file to activate app.

--- google_keep/models.py
+++ google_keep/models.py
@@ -3,18 +3,18 @@ from django.db import models
 # Create your models here.
 class Tag(models.Model):
     """Tag what will be assigned to each Note."""
-
     name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
+        """Return a string representation of the model."""
         return self.name
 class Note(models.Model):
     """Note created by user."""
-
     title = models.CharField(max_length=255)
     body = models.TextField()
     date_added = models.DateField(auto_now_add=True)
     tags= models.ManyToManyField(Tag, blank=True) #Allows notes without a Tag.
 
     def __str__(self):
+        """Return a string representation of the model."""
         return self.title
