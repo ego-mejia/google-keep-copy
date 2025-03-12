c6da6e490a549431d12a5576d540297a1526e2f2
Fernando Mejia Laguna <mejia190899@gmail.com>
2025-03-11 20:28:34 -0600

    Added models for Tag & Notes.

--- google_keep/models.py
+++ google_keep/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    """Tag what will be assigned to each Note."""
+
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+class Note(models.Model):
+    """Note created by user."""
+
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    date_added = models.DateField(auto_now_add=True)
+    tags= models.ManyToManyField()
+
+    def __str__(self):
+        return self.title
