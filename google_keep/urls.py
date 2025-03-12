c161e27acc8f4c9434abf8e2438f36f1deb0b298
Fernando Mejia Laguna <mejia190899@gmail.com>
2025-03-11 20:55:04 -0600

    Added app url in project urls file.

--- google_keep/urls.py
+++ google_keep/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import note_list
 
 urlpatterns = [
-    path('', note_list, name='note_list'),
+    path('', note_list, name='note_list'), # '' represents the base url on the app, the index.
 ]
